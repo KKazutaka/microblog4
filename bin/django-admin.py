d71d8188641a4c712d6a31181a7dfb9c8f4b12d9
kk <you@example.com>
2019-10-27 18:14:53 +0900

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kk/django/1026_microblog/microblog/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from .views import BlogListView,BlogDetailView,BlogCreateView,BlogUpdateView,BlogDeleteView
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('',BlogListView.as_view(), name="index"),
+    path('<int:pk>',BlogDetailView.as_view(), name="detail"),
+    path('create',BlogCreateView.as_view(),name='create'),
+    path('<int:pk>/update',BlogUpdateView.as_view(),name="update"),
+    path('<int:pk>/delete',BlogDeleteView.as_view(),name='delete'),
+    
+    path('login/',LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout',LogoutView.as_view(template_name="logout.html"), name="logout"),
+
+]
